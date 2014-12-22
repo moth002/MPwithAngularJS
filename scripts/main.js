4eee2e0821931b31183bd5c4907dffbec389da80
moth002 <moth002@gmail.com>
2014-12-23 09:42:01 +1300

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module("myApp", ['ngRoute']);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: "views/main.html",
+        controller: "AppCtrl"
+    });
+    $routeProvider.when('/nurse', {
+        templateUrl: "views/nurse.html"
+    });
+    //$routeProvider.otherwise({
+    //                redirectTo: '/'
+    //});
+}]);
+
+app.controller("AppCtrl", ['$scope', function ($scope) {
+    $scope.model = {
+        message: "Scan or enter your ID"
+    }
+}]);
