563fdb0d82923dff1e30a8502af09b029ee07874
moth002 <moth002@gmail.com>
2014-12-24 12:33:02 +1300

    moved from JQM to ionic

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,53 @@
-﻿var app = angular.module("myApp", ['ngRoute']);
+﻿angular.module("myApp", ['ionic', 'ngRoute'])
 
-app.config(["$routeProvider", function ($routeProvider) {
-    $routeProvider.when('/', {
+//.config(function ($compileProvider) {
+//    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+//})
+
+//.run(function ($ionicPlatform) {
+//    $ionicPlatform.ready(function () {
+//        if (window.StatusBar) {
+//            // org.apache.cordova.statusbar required
+//            StatusBar.styleDefault();
+//        }
+//    });
+//})
+
+.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when("/", {
         templateUrl: "views/main.html",
         controller: "AppCtrl"
     });
-    $routeProvider.when('/nurse', {
-        templateUrl: "views/nurse.html"
+    $routeProvider.when("/nurse", {
+        templateUrl: "views/nurse.html",
+        controller: "NurseCtrl"
     });
-    //$routeProvider.otherwise({
-    //                redirectTo: '/'
-    //});
-}]);
+    $routeProvider.otherwise({
+                    redirectTo: '/'
+    });
+}])
 
-app.controller("AppCtrl", ['$scope', function ($scope) {
+
+//app.controller("MainCtrl", ['$scope', function ($scope) {
+//    alert("Hello");
+//    $scope.$on("$includeContentLoaded", function () {
+//        $("#mainDiv").trigger("create");
+//    });
+//}]);
+
+.controller("AppCtrl", ['$scope', function ($scope) {
     $scope.model = {
         message: "Scan or enter your ID"
     }
-}]);
+
+    //$scope.$on("$viewContentLoaded", function () {
+    //    $("#mainDiv").trigger("create");
+    //});
+
+}])
+
+//.controller("NurseCtrl", ['$scope', function ($scope) {
+//    $scope.model = {
+//        message: "Scan the patient's wristband or enter the NHI"
+//    }
+//}]);
