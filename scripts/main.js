2d32093df2b85e9e2502ff9270fd326f61be37a6
moth002 <moth002@gmail.com>
2015-01-05 09:46:24 +1300

    changed nurse to user

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,18 @@
 ﻿angular.module("myApp", ['ionic', 'ngRoute'])
 
+//.run(function($ionicPlatform) {
+//    $ionicPlatform.ready(function() {
+//        //Hide the accessory bar by default (remove this to show the accesory bar above the keyborad
+//        // for form input)
+//        if (window.cordova && window.cordova.plugins.Keyboard) {
+//            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//        }
+//        if (window.StatusBar) {
+//            StatusBar.styleDefault();
+//        }
+//    });
+//})
+
 .config(["$routeProvider", function ($routeProvider) {
     $routeProvider.when("/", {
         templateUrl: "views/main.html",
