1282c8357a8d0bd4a998597ae99707eefb794cf4
moth002 <moth002@gmail.com>
2015-03-16 12:03:34 +1300

    Forgot to add the JS file to the repository, thats why it was not working

---
+++ controllers/SideMenuBtnCtrl.js
@@ -0,0 +1,22 @@
+﻿angular.module('myApp')
+    .controller("SideMenuBtnCtrl", [
+        '$scope', 'cordovaReadyService', '$ionicSideMenuDelegate',
+            function ($scope, cordovaReadyService, $ionicSideMenuDelegate) {
+
+                $scope.btnHome = function () {
+                    $ionicSideMenuDelegate.toggleLeft();
+                    window.location = '#/';
+                };
+
+                $scope.btnBluetooth = function () {
+                    cordovaReadyService(bluetoothSerial.list(function (devices) {
+                        devices.forEach(function(device) {
+                            alert(device.name);
+                        });
+                    }, function(reason) {
+                        alert(reason);
+                    }));
+                };
+
+            }
+    ]);
