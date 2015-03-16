34f3b13cec2c6701253bb709c660d2af7b3a5387
moth002 <moth002@gmail.com>
2015-03-16 13:51:54 +1300

    Forgot the service file again

---
+++ services/NotificationDlgService.js
@@ -0,0 +1,21 @@
+﻿angular.module('myApp')
+    .factory('notificationDlgService', ['cordovaReadyService', function (cordovaReadyService) {
+
+        function alertDismissed() {
+            // do something
+        }
+
+        var notificationDlg = {
+
+            alertDialog: function (message, title) {
+                return cordovaReadyService(navigator.notification.alert(
+                       message,         // message
+                       alertDismissed,  // callback
+                       title,           // title
+                       'Done'           // buttonName
+                   ));
+            }
+        };
+
+        return notificationDlg;
+    }]);
