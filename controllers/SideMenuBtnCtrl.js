46e02fe85f172b8dfbb879daf74f97ef5f1cb754
moth002 <moth002@gmail.com>
2015-03-16 17:04:33 +1300

    Added the toggle for device activiation

--- controllers/SideMenuBtnCtrl.js
+++ controllers/SideMenuBtnCtrl.js
@@ -3,6 +3,20 @@
         '$scope', 'cordovaReadyService', '$ionicSideMenuDelegate',
             function ($scope, cordovaReadyService, $ionicSideMenuDelegate) {
 
+                $scope.ctlDeviceActive = {
+                    value: true
+                }
+
+                $scope.deviceActiveChanged = function () {
+                    // ------------------------------------------------------------------
+                    // An alert to get the admin password
+                    // A call to the WebServer to confirm the password
+                    // and register or deregister a device.
+                    // ------------------------------------------------------------------
+                    alert("You have to be an admin");
+                    $scope.ctlDeviceActive.value = true;
+                }
+
                 $scope.btnHome = function () {
                     $ionicSideMenuDelegate.toggleLeft();
                     window.location = '#/';
